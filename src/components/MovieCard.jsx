8c18760d1fc844f388da39376f186618f8aa4d64
Noriuki <lanezupiroli@gmail.com>
2021-04-14 01:02:22 -0300

    (refactor)[cine-busca]: style changes css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
 const MovieCard = ({ title, posterUrl, release, movieID }) => (
-  <Link to={`movie/${movieID}`}>
-    <div className="movie-card">
+  <Link to={`movie/${movieID}`} className="movie-card-link">
+    <div>
       <img className="movie-card__poster" src={posterUrl} alt={title} />
       <div className="movie-card-info">
         <h3 className="movie-card-info__title">{title}</h3>
--- src/views/MoviePage.jsx
+++ src/views/MoviePage.jsx
@@ -26,27 +26,39 @@ const MoviePage = () => {
       <div className="page-container__content">
         <div className="movie-info">
           <h1 className="movie-info__title">{movie.Title}</h1>
-          <div className="movie-info-misc">
-            <div className="movie-info-misc__field">
-              <p>Release Date</p>
-              <p>{movie.Year}</p>
+          <div className="movie-page">
+            <div className="movie-column">
+              <div className="movie-info-misc">
+                <div className="movie-info-misc__field">
+                  <h4>Release Date</h4>
+                  <p>{movie.Year}</p>
+                </div>
+                <div className="movie-info-misc__field">
+                  <h4>Genre</h4>
+                  <p>{movie.Genre}</p>
+                </div>
+                <div className="movie-info-misc__field">
+                  <h4>Director</h4>
+                  <p>{movie.Director}</p>
+                </div>
+              </div>
+              <p className="movie-info__label"> Movie Casting </p>
+              <div className="movie-casting">
+                <h4 className="movie-info__casting">{movie.Actors}</h4>
+              </div>
             </div>
-            <div className="movie-info-misc__field">
-              <p>Genre</p>
-              <p>{movie.Genre}</p>
-            </div>
-            <div className="movie-info-misc__field">
-              <p>Director</p>
-              <p>{movie.Director}</p>
+            <div className="movie-column">
+            <img
+              className="movie-info__poster"
+              src={movie.Poster}
+              alt={movie.Title}
+              />
             </div>
           </div>
-          <img className="movie-info__poster" src={movie.Poster} alt={movie.Title} />
-          <p className="movie-info__label"> Movie Casting </p>
-          <h4 className="movie-info__casting">{movie.Actors}</h4>
-          <p className="movie-info__label"> Movie Plot </p>
-          <div className="movie-info__plot">
-            <h3>{movie.Plot}</h3>
-          </div>
+        </div>
+        <p className="movie-info__label"> <strong>Movie Plot</strong> </p>
+        <div className="movie-info__plot">
+          <h4>{movie.Plot}</h4>
         </div>
       </div>
     </div>
